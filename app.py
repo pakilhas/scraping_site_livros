93f1446bff58b1d9388a1be93389df5646e2c5e5
pakilhas <pakilhas@gmail.com>
2024-03-11 16:43:09 -0300

    scrip para webscraping, falta resolver problema de fechamento do navegador e como salvar as informações

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service  # Importe a classe correta para o serviço do Edge
+from selenium.webdriver.common.by import By
+import time
+
+# Iniciar o Microsoft Edge WebDriver
+service = Service()  
+options = webdriver.ChromeOptions()
+
+# Iniciar o navegador Edge
+driver = webdriver.Chrome(service=service, options=options)
+
+#url que vamos fazer o scraping
+url = 'https://books.toscrape.com/'
+driver.get(url)
+
+# meu navegador está fechando, deixei por enquanto até resolver
+time.sleep(30) 
+
+
+elementos_a = driver.find_elements(By.TAG_NAME, 'a')
+for i in range(58, 95, 2):
+    if i < len(elementos_a):
+        titulo = elementos_a[i].get_attribute('title')
+        print(f"Título do elemento {i}: {titulo}")
+# Fechar o navegador
+driver.quit()
